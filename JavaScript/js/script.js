86836108a67140443cf772f8b754273d76f24a5d
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-01-24 19:08:41 +0500

    complete some tasks

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-let userName = prompt('Введите ваше имя','');
-
-if (userName === 'admin'){
-    let userPassword = prompt ('Введите пароль', '');
-    if (userPassword === '1234'){
-        alert('Пароль верный');
-    }else if (userPassword === '' || userPassword === null){
-        alert('Отменено');
-    }else{
-        alert('Неверно');
-    }
-}else if (userName === '' || userName === null){
-    alert('Отменено');
-}else{
-    alert('Ты кто ?');
-}
+let user;
+let user2
+alert(user??user2);
