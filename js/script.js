fd2ae56f945e3cef3479558f49bf2b0a99adb7d4
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-01-28 15:32:25 +0500

    switch конструкция

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-hey:
-for (let i = 2; i < 10; i++){
-    for (let j = 2; j < 10, j++;){
-        result = i * j;
-        console.log(resul);
-    }
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+switch (number){
+    case 0:
+        alert('Вы ввели число 0');
+        break;
+    case 1:
+        alert('Вы ввели число 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
 }
