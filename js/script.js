94cb7786e0f0968b5e851e7bd83a8418a2fdfe53
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-06 20:52:47 +0500

    Objects

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
 'use strict';
 
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-};
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
 
+// function func(){
+//   for (let key in menu){
+//     if (typeof menu[key] == 'number'){
+//       menu[key]*=2;
+//     }
+//   }
+// }
+// func();
+// console.log(menu);
 
-  for (let key in menu){
-    if (typeof menu[key] == 'number'){
-      menu[key]*=2;
-    }
-  }
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
 
+// let sum = 0;
 
-console.log(menu);
+// for (let key in salaries){
+//   sum += salaries[key];
+// }
+// console.log(sum);
+
+let obj = {
+  name:'Daniil',
+}
+function isEmpty(obj){
+  for (let key in obj){
+    return false
+  } return true
+}
+console.log( isEmpty(obj) )
