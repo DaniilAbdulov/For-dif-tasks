6b3d3e45243de0fd506ffcfcd777683bcefde11b
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-07 19:30:29 +0500

    2413. Smallest Even Multiple

--- js/script.js
+++ js/script.js
@@ -1,40 +1,9 @@
 'use strict';
 
-// let menu = {
-//   width: 200,
-//   height: 300,
-//   title: "My menu"
-// };
 
-// function func(){
-//   for (let key in menu){
-//     if (typeof menu[key] == 'number'){
-//       menu[key]*=2;
-//     }
-//   }
-// }
-// func();
-// console.log(menu);
+var smallestEvenMultiple = function(n) {
+    return  (n % 2 == 0) ? (n) : (n*2)
+};
+console.log( smallestEvenMultiple(10) );
 
-// let salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130
-// }
 
-// let sum = 0;
-
-// for (let key in salaries){
-//   sum += salaries[key];
-// }
-// console.log(sum);
-
-let obj = {
-  name:'Daniil',
-}
-function isEmpty(obj){
-  for (let key in obj){
-    return false
-  } return true
-}
-console.log( isEmpty(obj) )
