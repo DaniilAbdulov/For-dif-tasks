e5eeba430177d802071c180ca60a7f6d4ab32f05
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-08 18:00:11 +0500

    Методы объекта и this

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 'use strict';
 
 
-var smallestEvenMultiple = function(n) {
-    return  (n % 2 == 0) ? (n) : (n*2)
-};
-console.log( smallestEvenMultiple(10) );
+// let calculator = {
+//     sum (){
+//         return this.a + this.b;
+//     },
+//     mul (){
+//         return this.a * this.b;
+//     },
+//     read(){
+//         this.a = + prompt ('a ?', '');
+//         this.b = + prompt ('b ?', '');
+//     }
+//   };
+  
+//   calculator.read();
+//   alert( calculator.sum() );
+//   alert( calculator.mul() );
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+      alert( this.step );
+      return this;
+    }
+  };
 
-
+  ladder.up().up().up().up().down().showStep().down().showStep();
