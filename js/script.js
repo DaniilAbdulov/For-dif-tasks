7daae9c80c9b11538590aa332f05121f025ce4cd
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-08 21:40:08 +0500

    'This'

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 'use strict';
 
 
-// let calculator = {
-//     sum (){
-//         return this.a + this.b;
-//     },
-//     mul (){
-//         return this.a * this.b;
-//     },
-//     read(){
-//         this.a = + prompt ('a ?', '');
-//         this.b = + prompt ('b ?', '');
-//     }
-//   };
+let calculator = {
+    sum (){
+        return this.a + this.b;
+    },
+    mul (){
+        return this.a * this.b;
+    },
+    read(){
+        this.a = + prompt ('a ?', '');
+        this.b = + prompt ('b ?', '');
+    }
+  };
   
-//   calculator.read();
-//   alert( calculator.sum() );
-//   alert( calculator.mul() );
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
+
 let ladder = {
     step: 0,
     up() {
