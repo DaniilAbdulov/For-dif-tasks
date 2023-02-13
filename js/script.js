e7c55e184f65c17dc2a7729477ec1c900f884348
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-02-13 19:00:19 +0500

    Codewars

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 'use strict';
 
-function powersOfTwo(n){
-    let arr =[];
-    for (let i = 0; i <= n; i++){
-      arr.push(2**i)
-    }
-    return arr;
-  }
-powersOfTwo(0)
-
-// assert.deepEqual(powersOfTwo(0), [1])
-// assert.deepEqual(powersOfTwo(1), [1, 2])
-// assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16])
+function sumStr(a,b) {
+  return +a + +b;
+}
+console.log(sumStr("4","5"));
