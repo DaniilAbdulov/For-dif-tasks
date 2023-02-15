c6fa63a26a61b5219dd1a5af56d53b931698390c
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-15 21:23:33 +0500

    String ends with?

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 'use strict';
 
-
-function numToString (num){
-  let string = 0;
-  for (let i = num.length - 1; i >= 0 ; i--){
-    string += +num[i];
-  }
-  return string
-}
-
-
-let a = numToString ('321');
-let b = numToString ('123');
-
-console.log(a == b);
-
+const summation = n => n * (n + 1) / 2;
+console.log(summation(2));
