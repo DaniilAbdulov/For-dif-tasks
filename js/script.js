b7f91e04a8fe6b263b13b7fb46af30090d5f9125
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-02-17 19:40:43 +0500

    Codewars

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 'use strict';
 
-function noSpace(x){
-    return x.replace(/\'_'/g, '\b')
- }
-   
-console.log(noSpace('he ll o'));
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+function nbYear(p0, percent, aug, p) {
+    
+    for (var years = 0; p0 < p; years++) {
+      p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+    }
+    return years
+  }
+console.log( nbYear(1500, 5, 100, 5000) )//15
