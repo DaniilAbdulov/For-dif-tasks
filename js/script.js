ee64a7bdfae84f699dbeff573afed6f56a41321c
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-18 16:30:48 +0500

    2. Add Two Numbers решено, но LeetCode выдает оши

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 'use strict';
-
-function correct(string){
-    for (let i = 0; i < string.length; i++){
-        
+// 2. Add Two Numbers. Проблема в том, что в 5 строке не работает n.reverser()
+var addTwoNumbers = function(l1, l2) {
+    function fromStringtoNumber(n){
+        let arr = n.reverse();
+        let arrR = arr.join('');
+        return +arrR;
     }
+    let one = fromStringtoNumber(l1);
+    let two = fromStringtoNumber(l2);
+    let str = one + two + '';
+    let arr = [];
+    for (let i = 0; i < str.length; i++){
+    arr.push(str[i]);
+    }
+    for (let i = 0; i < arr.length; i++){
+    arr[i] = +arr[i];
+    }
+    return arr.reverse();
     
-}
-console.log(correct('L0ND0N'));
+};
+
+
+console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]));
+
