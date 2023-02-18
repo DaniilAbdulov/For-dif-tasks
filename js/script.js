8e9d317ca4fdc7da9d4f837164487e4a51140cb8
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-18 17:04:10 +0500

    Решить!

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
 'use strict';
-//2.Add two Numbers в логе все работает но LeetCode ругается на ошибку в 5 строке n.reverse() не является функцией
-var addTwoNumbers = function(l1, l2) {
-    function fromStringtoNumber(n){
-        let arr = n.reverse().join('');
-        return +arr;
-    }
-    let one = fromStringtoNumber(l1);
-    let two = fromStringtoNumber(l2);
-    let str = one + two + '';
-    let arr = [];
-    for (let i = 0; i < str.length; i++){
-    arr.push(str[i]);
-    }
-    for (let i = 0; i < arr.length; i++){
-    arr[i] = +arr[i];
-    }
-    return arr.reverse();
-    
+//896.Monotonic Array
+var isMonotonic = function(nums) {
+    if (nums[0] < nums.length-1) {
+        for (let i = 0; i <nums.length; i++){
+            if (nums[i+1]<=i)continue;
+            return 'он монотонный возрастает'
+        }
+    }else if(nums[0] > nums.length-1){
+        for (let i = 0; i <nums.length; i++){
+            if (nums[i]<=i+1)continue;
+            return 'он монотонный убыв'
+        }
+    }else false  
 };
-
-console.log( addTwoNumbers([1,2,3],[4,5,6]) );
+console.log( isMonotonic([1,3,2]) );
