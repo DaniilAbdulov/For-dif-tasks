4aadbab5b56705368d86fb4e1bdea6539682b464
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-19 18:51:36 +0500

    24 Easy

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 'use strict';
 //896.Monotonic Array
-var mergeTwoLists = function(list1, list2) {
-    let arr = []
-    let newArr = arr.concat(list1, list2)
-    return newArr.sort( (a,b) => a -b )
+var smallerNumbersThanCurrent = function(nums) {
+    let arr = [];
+    for (let i = 0; i < nums.length; i++){
+        let sum = 0;
+        for (let j = 0; j < nums.length; j++){
+            if (nums[i] > nums[j]){
+                sum+=1;
+            }
+        }arr.push(sum);
+    }
+    return arr;
 };
-console.log( mergeTwoLists([1,2,4],[1,3,4]) );
+
+
+console.log( smallerNumbersThanCurrent([8,1,2,2,3]));
 
 
