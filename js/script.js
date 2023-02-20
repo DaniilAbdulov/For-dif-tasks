6172dd73c38822e0d68ff6f91820431852953a1e
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-20 19:42:29 +0500

    leetCode

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 'use strict';
 //896.Monotonic Array
-var smallerNumbersThanCurrent = function(nums) {
-    let arr = [];
+var maxOperations = function(nums, k) {
     for (let i = 0; i < nums.length; i++){
-        let sum = 0;
-        for (let j = 0; j < nums.length; j++){
-            if (nums[i] > nums[j]){
-                sum+=1;
-            }
-        }arr.push(sum);
+        if (nums[i] + nums[i+1] == k) {
+            
+        }
     }
-    return arr;
 };
 
 
-console.log( smallerNumbersThanCurrent([8,1,2,2,3]));
+console.log( maxOperations([1,2,3,4] , 5));
 
 
