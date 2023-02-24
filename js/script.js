5b4a23db9b653e30f31c520d6c001461e75ed937
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-24 20:40:12 +0500

    Codewars

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
 'use strict';
 
-function prod2sum(a, b, c, d) {
-  let arr = []
-  let n = (Math.pow(a,2) + Math.pow(b,2)) * (Math.pow(c,2)+ Math.pow(d,2));
-  for (let i = 1; i < n; i++){
-    for (let j = 1; j < n; j++){
-      if ((Math.pow(i,2) + Math.pow(j,2)) == n){
-        arr.push(i)
-        arr.push(j)
-      }
-    }
-  }
-  if (arr.length > 4){
-    let newArr = arr.slice(0,4)
-    let one = newArr.slice(0,2)
-    let two = newArr.slice(2)
-    let result = [one, two]
-    return result
-  }else {
-    return [arr.slice(0,2)]
-  }
+function longestRepetition(s) {
+  let str = s;
 
+  let target = 'Иа'; // цель поиска
+  
+  let pos = 0;
+  while (true) {
+    let foundPos = str.indexOf(target, pos);
+    if (foundPos == -1) break;
+  
+    alert( `Найдено тут: ${foundPos}` );
+    pos = foundPos + 1; // продолжаем со следующей позиции
+  }
 }
-console.log( prod2sum(1, 1, 3, 5) );
+console.log( longestRepetition("aaaabb") );
