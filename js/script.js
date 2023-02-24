0a4cf7f4dceb60693282e1386e6684b754b59ff2
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-02-24 18:42:15 +0500

    Integers: Recreation Two

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 'use strict';
 
-var singleNumber = function(nums) {
-  let arr =[]
-  for (let i = 0; i < nums.length; i++) {
-    let val = nums[i];
-    nums.slice(val)
-    if (nums.includes(val)){
-        i++
+function prod2sum(a, b, c, d) {
+  let arr = []
+  let n = (Math.pow(a,2) + Math.pow(b,2)) * (Math.pow(c,2)+ Math.pow(d,2));
+  for (let i = 1; i < n; i++){
+    for (let j = 1; j < n; j++){
+      if ((Math.pow(i,2) + Math.pow(j,2)) == n){
+        arr.push(i)
+        arr.push(j)
+      }
     }
-    arr.push(nums.indexOf(val))
-  }return arr
-  
+  }
+  if (arr.length > 4){
+    let newArr = arr.slice(0,4)
+    let one = newArr.slice(0,2)
+    let two = newArr.slice(2)
+    let result = [one, two]
+    return result
+  }else {
+    return [arr.slice(0,2)]
+  }
 
 }
-console.log( singleNumber([2,2,1]) );
+console.log( prod2sum(1, 1, 3, 5) );
