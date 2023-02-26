65a43c958e95b3471326f10ce8d315a7b9beacd1
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-02-26 09:54:35 +0500

    New file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
 'use strict';
-
-
-
-function pizza(a, b) {
-    function area(d){
-        return 3.14 * Math.pow(d/2, 2)
-    }
-    let costOfBig = a / area(+prompt(`Диаметр большой пиццы`));
-    let costOfSmall = b / area(+prompt(`Диаметр маленькой пиццы`));
-    return (costOfBig < costOfSmall) ? `Выгоднее заказать 32см` : `Выгоднее заказать 25см`
-
+function name (str){
+  return str
 }
-
-
-alert( pizza(+prompt(`Стоимость большой пиццы`), +prompt(`Стоимость маленькой пиццы`)) );
+console.log( name('hello') );
 
 
