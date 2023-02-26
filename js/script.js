5037734d3ede7e63be6dc47da078be87fc062a2f
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-02-26 11:58:44 +0500

    A few tasks

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 'use strict';
-function removeEveryOther(arr){
-  let newArr = []
-  for (let char of arr){
-    if (arr.indexOf(char)%2==0){
-      newArr.push(char)
-    }
-  }
-  return newArr
+function reverseWords(str) {
+  return +str.toString(16)
 }
-console.log( removeEveryOther(["3","n","y","r","q","t","s","3","7","h","r","a","s","x","q","g","a","i","1"]) );
+console.log( reverseWords('1011') );
 
-//'3', 'y', 'q', 's', '7', 'r', 's', 'q', 'a', '1'
+
