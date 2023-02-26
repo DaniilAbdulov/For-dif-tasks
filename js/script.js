e4f246a0f1d0522e7c23353be458fab2c453e88a
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-02-26 10:59:33 +0500

    cc

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 'use strict';
-function name (str){
-  return str
+function removeEveryOther(arr){
+  let newArr = []
+  for (let char of arr){
+    if (arr.indexOf(char)%2==0){
+      newArr.push(char)
+    }
+  }
+  return newArr
 }
-console.log( name('hello') );
+console.log( removeEveryOther(["3","n","y","r","q","t","s","3","7","h","r","a","s","x","q","g","a","i","1"]) );
 
-
+//'3', 'y', 'q', 's', '7', 'r', 's', 'q', 'a', '1'
