abaf86c03e4e233a06ee896fff595084ce58524b
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-02-28 18:51:03 +0500

    Node js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ function switcher(x){
     let aplpabet = 'zyxwvutsrqponmlkjihgfedcba!? '
     return x.map(item=>aplpabet.charAt(+item-1)).join('')
   }
-console.log( switcher(['24', '12', '23', '22', '4', '26', '9', '8']) );
+
 
 
