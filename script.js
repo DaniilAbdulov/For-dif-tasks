e28db909d2bc9603ebd3154cc8ad586cc93d0286
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-02 21:46:47 +0500

    tasks from CodeWars n LeetCode

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 'use strict';
-function longestConsec(strarr, k) {
-  let arr = []
-  for (let i = 0;i < strarr.length-1;i++){
-    arr.push(strarr[i]+strarr[i+1])
-  }
-  return Math.max(...arr.map(item=>item.length))
-}
-console.log(longestConsec (["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"] ,2));
+var containsDuplicate = function(nums) {
+    let set = new Set (nums)
+    let n = Array.from(set);
+    return nums.length != n.length
+};
+console.log( containsDuplicate([1,2,3,1]) );
 
