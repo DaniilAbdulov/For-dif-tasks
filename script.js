7850a2a40c49dee46584c2da8bc421b7f52da8f8
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-04 14:41:04 +0500

    reverse

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 'use strict';
-
-var twoSum = function(nums, target) {
-  // if(nums[0] == 0 || target == 0){
-  //   return [1,2]
-  // }
-  let left = 0;
-  let right = nums.length-1;
-  while(left < right){
-    sum = nums[left] + nums[right]
-    if (sum == target){
-      return [left+1,right+1]
-    }else if (sum < target){
-      left += 1
-    }else {
-      right -= 1
-    }
-  }return [nums[left],nums[right]]
-
-
-
+var reverse = function(x) {
+    let str = x.toString()
+    let reverse = str.split('').reverse().join('')
+    if (str[0] == '-'){
+      return reverse
+    }return  +reverse
+    
 };
-console.log( twoSum([3,24,50,79,88,150,345], 200));
+
+console.log( reverse( -123  ));
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,5 @@
-i
+  let min = Math.min(...arr)
+  let ind = prices.indexOf(min)
+  let newArr = arr.slice(ind);
+  
+  return Math.max(...newArr)-min
