656c7ac61fc3d76c26bf037c8b59e1477713f3c5
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-04 13:59:58 +0500

    two sum ||

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 'use strict';
-function nthSmallest(...arrays, n ) {
-    return n
-  }
-console.log( nthSmallest([1,5], [2], [4,8,9], 4));
 
+var twoSum = function(nums, target) {
+  // if(nums[0] == 0 || target == 0){
+  //   return [1,2]
+  // }
+  let left = 0;
+  let right = nums.length-1;
+  while(left < right){
+    sum = nums[left] + nums[right]
+    if (sum == target){
+      return [left+1,right+1]
+    }else if (sum < target){
+      left += 1
+    }else {
+      right -= 1
+    }
+  }return [nums[left],nums[right]]
+
+
+
+};
+console.log( twoSum([3,24,50,79,88,150,345], 200));
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-.map(item=>+item)
+i
