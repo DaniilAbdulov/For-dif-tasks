321ac92cc7b467c5286fe1fa3e290a7777f5aaa5
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-08 19:22:26 +0500

    CodeWars

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 'use strict';
-
-function findUniq(arr) {
-  if(arr[0]==1){
-    return +0
-    
-  }else if(arr.length==1){
-    return +arr[0]
-  }
-  let a = Array.from(new Set(arr))
-  let indOne = arr.indexOf(a[0])
-  let indTwo = arr.indexOf(a[1])
-  a.splice(indOne,1).splice(indTwo,1)
-  return a[0]
+function countSheeps(arrayOfSheep) {
+  return arrayOfSheep.map(item=>+item).reduce( (s,c)=>s+c,0 )
 }
-console.log( findUniq([1]) );
+console.log( countSheeps([true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]) );
