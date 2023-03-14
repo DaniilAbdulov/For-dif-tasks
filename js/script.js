fbdf4d230cea8a3adb0ad44561cb3bce860ce7cf
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-14 18:33:47 +0500

    CodeWars

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
 'use strict';
 
-function findOutlier(integers){
-  let arr = Array.from(new Set(integers))
-  if(arr[0]%2==0 && arr[1]%2==0){
-    for (let val of arr){
-      if (val % 2 == 1){
-        return val
-      }
-    }
-  }else {
-    for (let val of arr){
-      if (val % 2 == 0){
-        return val
-      }
-    }
-  }
+function getCount(str) {
+  const words  = ['a','e','i','o','u'];
+  let arr = str.split('')
+  return arr.filter(item=>words.includes(item)).length;
 }
-
-console.log( findOutlier([1, 2, 3]) );
+console.log( getCount("abracadabra") );
