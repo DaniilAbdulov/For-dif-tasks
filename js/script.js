fe93af3ec3019938355f710ae75e604b283da7ae
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-15 19:34:39 +0500

    codewars

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
-function getCount(str) {
-  const words  = ['a','e','i','o','u'];
-  let arr = str.split('')
-  return arr.filter(item=>words.includes(item)).length;
-}
-console.log( getCount("abracadabra") );
+var maximum69Number  = function(num) {
+    let arr = (num+'').split('').map(item=>+item)
+    for (let i = 0; i < arr.length;i++){
+      if (arr[i] == 9)continue;
+      arr[i] = 6
+      return arr
+    }
+};
+console.log( maximum69Number(9669) );
