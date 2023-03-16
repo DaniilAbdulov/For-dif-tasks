4ba764b5de244df93aaae92169902cc5a897a0a4
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-16 19:17:49 +0500

    codewars

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var maximum69Number  = function(num) {
-    let arr = (num+'').split('').map(item=>+item)
-    for (let i = 0; i < arr.length;i++){
-      if (arr[i] == 9)continue;
-      arr[i] = 6
-      return arr
-    }
-};
-console.log( maximum69Number(9669) );
+function wave(str){
+  let arr = str.split('');
+  let answer = []
+  for (let i = 0; i < arr.length; i++){
+    let letter = arr[i].toUpperCase();
+    
+  }
+  return answer
+  
+}
+ console.log( wave('hello') )
