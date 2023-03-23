773de0266f0ffa4ed147f1253649adeebeb832da
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-23 20:24:18 +0500

    codewars

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 'use strict';
 
-function replicate(times, number) {
-	let arr = [];
-  if (times == 1){
-    return times
-  }else{
-    return arr.push(replicate(times-1,number))
+function explode(x){
+  function arr(n){
+    let arr = [];
+    for (let i = 0; i < n;i++){
+      arr.push(x)
+    }
+    return arr;
   }
+  return (typeof x[0] == 'string' || typeof x[1] == 'string') ? 'Void!' :
+  (x.includes(0)) ? [] : 
+  (typeof x[0] == 'number') ? arr(x[0]) :
+  (typeof x[1] == 'number') ? arr(x[1]) :
+  (typeof x[0] == 'number' || typeof x[1] == 'number') ? arr(x[0] + x[1]) : '';
 }
-console.log( replicate(3,1) );
+console.log( explode(['c', 5]) );
 
 
