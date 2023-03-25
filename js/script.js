a4a168443c35c7078f376d1b907ce2fb20dbdfc0
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-03-25 08:27:22 +0500

    CodeWars

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 'use strict';
-
-function initializeNames(name){
-  let arr = name.split(' ');
-  if (arr.length <= 2){
-    return name
-  }else{
-    let newArr = arr.slice(1).reverse().slice(1).reverse();
-    let elem = []
-    for (let i = 0;i < newArr.length;i++){
-      elem.push(newArr[i][0]+'.')
-    }
-    return arr[0] + ' ' +elem.join(' ') + ' ' + arr.at(-1)
-  }
+function findUniq(arr) {
+  let set = new Set(arr);
+  let newArr = Array.from(set);
+  let one = arr.filter(item=>item == newArr[0])
+  let two = arr.filter(item=>item == newArr[1])
+  return (two.length < one.length) ? two[0] : one[0];
 }
-console.log( initializeNames('Lois Mary Lane') );
+console.log(  findUniq([ 1, 1, 1, 2, 1, 1 ]))
 
 
