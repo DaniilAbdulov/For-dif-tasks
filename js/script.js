58548d3cb0263d4f5edb2ac288857a0e6a173b3f
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-03-27 20:39:01 +0500

    Codewars

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 'use strict';
-function findUniq(arr) {
-  let set = new Set(arr);
-  let newArr = Array.from(set);
-  let one = arr.filter(item=>item == newArr[0])
-  let two = arr.filter(item=>item == newArr[1])
-  return (two.length < one.length) ? two[0] : one[0];
-}
-console.log(  findUniq([ 1, 1, 1, 2, 1, 1 ]))
+const sumNested = l => {
+    if(Array.isArray(l)){
+      return l.reduce( (a,b)=> a+b,0 )
+    }else{
+      let sum = 0;
+      for (let sub of Object.entries(l)){
+        sum += sumNested(sub)
+      }
+      return sum
+    }
+};
+console.log(sumNested([10,[[10],10],[10]]));
 
 
