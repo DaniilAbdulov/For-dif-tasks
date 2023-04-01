b55448eb1901793e8cb883697a9b7349557d8e3e
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-04-01 11:36:48 +0500

    LeetCode

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 'use strict';
-var findErrorNums = function(nums) {
-   let arr = [];
-    for(let i = 0; i  < nums.length;i++){
-      if(nums[i] == nums[i+1]){
-        arr.push(nums[i])
-        arr.push(nums[i]+1)
+var sortArrayByParity = function(nums) {
+    let arrEven = [];
+    let arrOdd = [];
+    for(let i = 0;i<nums.length;i++){
+      if(nums[i] % 2 == 0){
+        arrEven.push(nums[i])
+      }else{
+        arrOdd.push(nums[i])
       }
     }
-    return arr
+    let ans = arrEven.concat(arrOdd);
+    return ans;
 };
-console.log( findErrorNums([1,1]) );
+    
+console.log( sortArrayByParity([3,1,2,4]) );
 
 
 
