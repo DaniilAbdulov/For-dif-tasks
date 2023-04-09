335bcbbe183da2e62273004dc36666577fc3d666
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-04-09 11:06:06 +0500

    Codewars

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 'use strict';
-var capitals = function (word) {
-	let arr = [...word].map(item=>item.codePointAt(0));
-  let newArr = arr.filter(item=> item > 64 && item < 91);
-  let answer = [];
-  for(let i = 0;i < newArr.length;i++){
-    answer.push(arr.indexOf(newArr[i]))
-  }
-  return answer;
-};
 
-console.log( capitals('CodEWaRs') )
+
+function sum(a){
+  return function(b){
+     return a + b
+  }
+}
+
+
+console.log(sum(1)(2));
