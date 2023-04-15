e1129fa6b494ddb8dd9d581822408ec0398c3793
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-04-15 13:10:03 +0500

    LeetCode

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 'use strict';
 
 
-var findTheLongestBalancedSubstring = function(s) {
-    let arr = [...s].sort( (a,b)=> a -b)
-    let count = 0;
-    for (let i = 0;i < arr.length;i++){
-      for(let j = arr.length-1; j > i; j--){
-        if(i != j)
-      }
-    }
-};
-
-console.log(findTheLongestBalancedSubstring('01000111'));
+function sum(n){
+  return function(a){
+    return n ** a;
+  }
+}
+let first = sum(4);
+console.log(first(20));
 
