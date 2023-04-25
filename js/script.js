09a80069ef201052057fca074e85c63fdc5c0881
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-04-25 05:56:43 +0500

    Merge branch 'main' of https://github.com/DaniilAbdulov/JavaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 'use strict'
 
-function convertTemperature(Celsius){
-  let arr = [Celsius + 273.15,Celsius*1.8 + 32];
-  return arr;
+function slow(x){
+  console.log(`Called with ${x}`);
+  return x;
 }
-console.log( convertTemperature(36.5) )
+function cachingDecorator(func) {
+  let cache = new Map();
+
+  return function(x) {
+    if (cache.has(x)) {   
+      console.log('я сработал') // если кеш содержит такой x,
+      return cache.get(x); // читаем из него результат
+    }
+
+    let result = func(x); // иначе, вызываем функцию
+
+    cache.set(x, result); // и кешируем (запоминаем) результат
+    console.log(cache)
+    return result;
+  };
+ 
+}
+slow = cachingDecorator(slow);
+console.log( slow(1) ); // slow(1) кешируем
+console.log( "Again: " + slow(1) ); // возвращаем из кеша
+
+console.log( slow(2) ); // slow(2) кешируем
+console.log( "Again: " + slow(2) ); // возвращаем из кеша
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,11 +1,4 @@
-    function work(obj){
-      let str = JSON.stringify(obj);
-      let arr = [];
-      for(let val of str){
-        arr.push(val.codePointAt())
-      }
-      return arr.reduce( (a,b)=>a+b,0 );
-    }
-    let arr1 = work(o1);
-    let arr2 = work(o2);
-    return (arr1 == arr2) ? true : false;
+if (cache.has(x)) {   
+    //   console.log('я сработал') // если кеш содержит такой x,
+    //   return cache.get(x); // читаем из него результат
+    // }
