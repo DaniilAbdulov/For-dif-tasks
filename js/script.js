0c2043d5153d5d231439f1181d2fd346e57c1ecf
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-04-30 10:55:42 +0500

    F.prototype

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 'use strict'
 
-let animal = {
-    eat() {
-      this.full = true;
-    }
-  };
-  
-  let rabbit = {
-    __proto__: animal
-  };
-  
-  rabbit.eat();
-  console.log(rabbit.full)
-  console.log(animal.full)
+function Rab(){
+  this.name = 'Daniil'
+  console.log(this.name)
+}
+
+let obj = new Rab();
+let obj2 = new obj.constructor();
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-{
-  value: "John"
-}
+true
