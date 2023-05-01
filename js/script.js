a41a4a1572b205459b5ab64892bcc918062f1507
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-01 13:55:04 +0500

    CodeWars

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 'use strict'
 
-function Rab(){
-  this.name = 'Daniil'
-  console.log(this.name)
-}
-
-let obj = new Rab();
-let obj2 = new obj.constructor();
+var average = function(salary) {
+    let arr = salary.sort( (a,b)=>a-b ).slice(0,salary.length-1)
+    let workarr = arr.slice(1,arr.length);
+    return workarr.reduce( (a,b)=>a+b,0 )/workarr.length;
+};
+console.log(average([4000,3000,1000,2000]))
