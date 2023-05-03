41f3062b6a2cfc6c4ab4caaf9b820b018b158fbe
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-03 20:31:04 +0500

    LeetCode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 'use strict'
-let obj = {
-    "0": 1,
-    0: 2
-   };
-   
-   alert( obj["0"]);
+let findDifference = (nums1, nums2)=> {
+    let arr1 = new Set(nums1);
+    let arr2 = new Set(nums2);
+    for(let val of arr1){
+        if(arr2.has(val)){
+            arr1.delete(val);
+            arr2.delete(val);
+        }
+    }
+    return [Array.from(arr1), Array.from(arr2)];
+};
+
+console.log( findDifference([1,2,3,3],[1,1,2,2]) )
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,1 +1,8 @@
-true
+    // for(let i = 0;i<nums2.length;i++){
+    //     let a = arr1.indexOf(arr1[i]);
+    //     let b = arr2.indexOf(arr1[i]);
+    //     if(arr2.includes(arr1[i])){
+    //         arr1.splice(a,1);
+    //         arr2.splice(b,1)
+    //     }
+    // }
