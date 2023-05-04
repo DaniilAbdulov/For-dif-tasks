27a5c1b8a0485d0a0406cf69a04a1778013677e1
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-04 06:47:28 +0500

    prototype

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 'use strict'
-let findDifference = (nums1, nums2)=> {
-    let arr1 = new Set(nums1);
-    let arr2 = new Set(nums2);
-    for(let val of arr1){
-        if(arr2.has(val)){
-            arr1.delete(val);
-            arr2.delete(val);
-        }
-    }
-    return [Array.from(arr1), Array.from(arr2)];
-};
 
-console.log( findDifference([1,2,3,3],[1,1,2,2]) )
+let arr = [1,2,3,4];
+
+Array.prototype.m = function(n){
+    return this.map(function(i){
+        return i + n
+    })
+}
+
+console.log(arr.m('-'))
