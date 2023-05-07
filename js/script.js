f4b2c50f58c00c44cede48fb1b5fd698e37d891a
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-07 12:56:32 +0500

    сссы

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 'use strict'
 
-Function.prototype.defer=function(ms){
-    setTimeout(this,ms);
+let worker = {
+  slow(min,max){
+    console.log(`Called with ${min},${max}`);
+    return min + max;
+  }
+};
+
+function cachingDecorator(func,hash){
+  let cache = new Map();
+  return function(){
+    let key = hash(arguments);
+
+    if(cache.has(key)){
+      return cache.get(key);
+    }
+    let result = func.apply(this,arguments);
+    cache.set(key,result);
+    return result;
+  }
+
+}
+function hash(args){
+  return args[0] + 'and' + args[1];
 }
 
-function f() {
-    console.log("Hello!");
-  }
-  
-f.defer(3000);
+worker.slow = cachingDecorator(worker.slow,hash);
+
+console.log( worker.slow(3,5) );
+console.log( worker.slow(3,5) );
