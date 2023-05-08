78a81585afe22b0a1d26ab128f37af7e362e07c3
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-08 14:05:51 +0500

    LeetCode

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 'use strict'
 
 
-
-let date = new Date();
-
-let a = date
-
-
-document.getElementById("code").innerHTML = a;
+var singleNumber = function(nums) {
+    let uniqNum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        console.log(uniqNum);
+        console.log(nums[i])
+        console.log('-');
+        uniqNum = uniqNum ^ nums[i];
+        console.log(uniqNum);
+        console.log(nums[i])
+        console.log('-');
+        
+    } return uniqNum;
+};
+singleNumber([2,2,1]) 
