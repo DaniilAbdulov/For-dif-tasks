9b8f59ab985bacc8352e5ebe2513aca5a789e996
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-05-09 10:30:09 +0500

    new theme

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
 'use strict'
 
+function Clock({ template }) {
+  
+  let timer;
 
-var singleNumber = function(nums) {
-    let uniqNum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        console.log(uniqNum);
-        console.log(nums[i])
-        console.log('-');
-        uniqNum = uniqNum ^ nums[i];
-        console.log(uniqNum);
-        console.log(nums[i])
-        console.log('-');
-        
-    } return uniqNum;
-};
-singleNumber([2,2,1]) 
+  function render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  this.stop = function() {
+    clearInterval(timer);
+  };
+
+  this.start = function() {
+    render();
+    timer = setInterval(render, 1000);
+  };
+
+}
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
+clock.stop();
