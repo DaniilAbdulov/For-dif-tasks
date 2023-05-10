2a094e94e37b84201d215b28e3b079ab0541f90b
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-10 21:14:26 +0500

    CodeWars

--- js/script.js
+++ js/script.js
@@ -1,39 +1,21 @@
 'use strict'
-function Clock({ template }) {
-  
-  let timer;
 
-  function render() {
-    let date = new Date();
 
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
-
-    let output = template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-      document.getElementById('code').innerHTML = output;
+function bouncingBall(h,  bounce, window) {
+  if(h > 0 && (bounce > 0 && bounce < 1) && h > window){
+    let count = 1;
+    let res = h;
+    while(res > window){
+      let p = 1;
+      res *= Math.pow(bounce,p)
+      count += 2;
+      p++
+    }
+    return count-2
+  }else{
+    return -1;
   }
-
-  this.stop = function() {
-    clearInterval(timer);
-  };
-
-  this.start = function() {
-    render();
-    timer = setInterval(render, 1000);
-  };
-
+ 
 }
-
-let clock = new Clock({template: 'h:m:s'});
-clock.start();
+console.log( bouncingBall(3.0, 1.0, 1.5) );
 
