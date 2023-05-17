3159db318615b6454d757c7187db120a52c49dde
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-17 21:04:49 +0500

    LeetCode

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict'
-
-var kidsWithCandies = function(candies, extraCandies) {
-    let max = Math.max(...candies);
-    let n = extraCandies;
-    let arr = candies.map(item=>item+n);
-
-    return arr.map(item=>item>=max);
-};
-
-console.log( kidsWithCandies([2,3,5,1,3],3) )
-
+class Sum{
+    constructor(num1,num2){
+        this.a = num1;
+        this.b = num2;
+    }
+    sum(){
+        return this.a + this.b;
+    }
+}
+let s = new Sum(1,2);
+console.log( s.sum() );
