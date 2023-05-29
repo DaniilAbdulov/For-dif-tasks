bffac4f29ed5f210c3bdf5cd619959dfd483e7d7
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-29 21:43:51 +0500

    cc

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 function maxProduct(a) {
-    let one;
-    let two;
-    let start = 0;
-    let end = a.length-1;
-    function search(arr){
-        one = arr[start];
-        two = arr[end];
-        while(start < end){
-            if(one > arr[start+1]){
-                continue
-            }else{
-                one = arr[start+1]
-            }
+    let max1 = a[0];
+    let max2;
+    for(let i = 1;i < a.length; i++){
+        if(a[i]>max1){
+            max2 = max1;
+            max1 = a[i];
+        }else if (a[i]>max2){
+            max2 = a[i]
         }
-        start += 1;
-        end -= 1;
     }
-    search(a)
-    return one
+    return max1 * max2
+  }
+
+console.log( maxProduct([56, 335, 195, 443, 6, 494, 252]) )
+
+function maxProduct(a) {
+    let max1 = a[0];
+    let max2 = Math.max(...a);
+    for(let i = 1;i < a.length; i++){
+        if(a[i] == max2)continue;
+        if(a[i]>max1){
+            max1 = a[i];
+        }
+    }
+    return max1 * max2;
   }
 
 console.log( maxProduct([56, 335, 195, 443, 6, 494, 252]) )
