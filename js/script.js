d2ef3236fb28830a416aaf33c773940793b971f3
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-05-30 06:28:25 +0500

    CallBacks

--- js/script.js
+++ js/script.js
@@ -1,29 +1,9 @@
-function maxProduct(a) {
-    let max1 = a[0];
-    let max2;
-    for(let i = 1;i < a.length; i++){
-        if(a[i]>max1){
-            max2 = max1;
-            max1 = a[i];
-        }else if (a[i]>max2){
-            max2 = a[i]
-        }
-    }
-    return max1 * max2
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+  
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error(`Не удалось загрузить скрипт ${src}`));
+  
+    document.head.append(script);
   }
-
-console.log( maxProduct([56, 335, 195, 443, 6, 494, 252]) )
-
-function maxProduct(a) {
-    let max1 = a[0];
-    let max2 = Math.max(...a);
-    for(let i = 1;i < a.length; i++){
-        if(a[i] == max2)continue;
-        if(a[i]>max1){
-            max1 = a[i];
-        }
-    }
-    return max1 * max2;
-  }
-
-console.log( maxProduct([56, 335, 195, 443, 6, 494, 252]) )
