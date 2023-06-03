002ca2cdbb509495f965e37d57e77bb002626f23
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-03 13:31:22 +0500

    promislar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-var twoSum = function(nums, target) {
-  let first = 0;
-  let second = 1;
-  let sum = 0
-  while(sum != target){
-    sum = nums[first] + nums[second];
-    first = second;
-    second +=1;
-  }
-  return [first-1,second-1]
-}
-console.log( twoSum([3,2,4], 6) );
+fetch('/article/promise-chaining/user.json')
+  .then(response => response.json())
+  .then(user => fetch(`https://api.github.com/users/${user.name}`))
+  .then(response => response.json())
+  .then(githubUser => new Promise(function(resolve, reject) { // (*)
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+
+    setTimeout(() => {
+      img.remove();
+      resolve(githubUser); // (**)
+    }, 3000);
+  }))
+  // срабатывает через 3 секунды
+  .then(githubUser => alert(`Закончили показ ${githubUser.name}`));
