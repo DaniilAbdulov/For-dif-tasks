44711e132b5d42cc14bd738f07dacbfd1697b946
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-03 13:41:48 +0500

    DateOfBirth

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
-fetch('/article/promise-chaining/user.json')
-  .then(response => response.json())
-  .then(user => fetch(`https://api.github.com/users/${user.name}`))
-  .then(response => response.json())
-  .then(githubUser => new Promise(function(resolve, reject) { // (*)
-    let img = document.createElement('img');
-    img.src = githubUser.avatar_url;
-    img.className = "promise-avatar-example";
-    document.body.append(img);
 
-    setTimeout(() => {
-      img.remove();
-      resolve(githubUser); // (**)
-    }, 3000);
-  }))
-  // срабатывает через 3 секунды
-  .then(githubUser => alert(`Закончили показ ${githubUser.name}`));
+let data = new Date(1976, 05, 03);
+function dateOfBirth(d){
+  return (Date.now() - d.getTime())/3.154e+10;
+}
+
+let answer = document.querySelector('.posts__list')
+answer.innerHTML = dateOfBirth(data).toFixed(0);
