eac4cf2139b58777d92a229b07cdcccb4fb1db0b
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-18 15:00:18 +0500

    asdad

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-var maximizeSum = function(nums, k) {
-  let end = Math.max(...nums)
+
+var sumOfUnique = function(nums) {
+  let cache = new Set();
   let sum = 0;
-  let i = 0;
-  while(i < k){
-    sum += end;
-    end++
-    i++
-    console.log(end)
+  for (let i = 0; i < nums.length; i++) {
+    if (!cache.has(nums[i])) {
+      cache.add(nums[i]);
+      sum += nums[i];
+    }
+    else {
+      sum -= nums[i];
+    }
   }
   return sum;
-
 };
 
-console.log( maximizeSum([4,4,9,10,10,9,3,8,4,2,5,3,8,6,1,10,4,5,3,2,3,9,5,7,10,4,9,10,1,10,4],6) )
+console.log( sumOfUnique([1,1,1,1,1,1]) );
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,2 +1,19 @@
-    console.log( str[start] );
-    console.log( str[end] );
+  function maximun(first,second){
+
+    while(second <= numbers.length){
+      max = numbers[first];
+      min = numbers[second];
+   
+      if(max > min)continue;
+      if(max < min){
+        max = min
+      }
+      
+  
+  
+      first++
+      second++
+      
+    }
+    return max
+  }
