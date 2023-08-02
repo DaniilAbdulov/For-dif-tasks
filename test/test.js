94a297e38567fd9dcb57ed004c26d561de1dc53a
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-08-02 06:48:32 +0500

    Разобраться

---
+++ test/test.js
@@ -0,0 +1,36 @@
+import { assert } from "assert";
+import replaceNth from "../replaceNth";
+
+describe("KataTest", function () {
+    it("BasicTests", function () {
+        assert.strictEqual(
+            replaceNth("Vader said: No, I am your father!", 2, "a", "o"),
+            "Vader soid: No, I am your fother!"
+        );
+        assert.strictEqual(
+            replaceNth("Vader said: No, I am your father!", 4, "a", "o"),
+            "Vader said: No, I am your fother!"
+        );
+        assert.strictEqual(
+            replaceNth("Vader said: No, I am your father!", 6, "a", "o"),
+            "Vader said: No, I am your father!"
+        );
+        assert.strictEqual(
+            replaceNth("Vader said: No, I am your father!", 0, "a", "o"),
+            "Vader said: No, I am your father!"
+        );
+        assert.strictEqual(
+            replaceNth("Vader said: No, I am your father!", -2, "a", "o"),
+            "Vader said: No, I am your father!"
+        );
+        assert.strictEqual(
+            replaceNth("Vader said: No, I am your father!", 1, "i", "y"),
+            "Vader sayd: No, I am your father!"
+        );
+
+        assert.strictEqual(
+            replaceNth("Luke cries: Noooooooooooooooo!", 6, "o", "i"),
+            "Luke cries: Noooooioooooioooo!"
+        );
+    });
+});
